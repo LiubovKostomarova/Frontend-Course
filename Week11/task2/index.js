daae14b44e6fee469571783a3176694e198014cc
Liubov Kostomarova <84984726+LiubovKostomarova@users.noreply.github.com>
2021-08-19 20:17:11 +0200

    Add files via upload

---
+++ Week11/task2/index.js
@@ -0,0 +1,31 @@
+// Add
+function add() {
+    const number1 = prompt('Enter the first summand', 0);
+    const number2 = prompt('Enter the second summand', 0);
+
+    alert(Number(number1) + Number(number2));
+}
+
+// Substract
+function substract() {
+    const number1 = prompt('Enter the substractor', 0);
+    const number2 = prompt('Enter the deductible', 0);
+
+    alert(Number(number1) - Number(number2));
+}
+
+// Multiply
+function multiply() {
+    const number1 = prompt('Enter the first summand', 0);
+    const number2 = prompt('Enter the second summand', 0);
+
+    alert(Number(number1) * Number(number2));
+}
+
+// Divide
+function divide() {
+    const number1 = prompt('Enter the first summand', 0);
+    const number2 = prompt('Enter the second summand', 0);
+
+    alert(Number(number1) / Number(number2));
+}
