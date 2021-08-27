807d6306dec55a5364b42efe7472df22b364063f
LiubovKostomarova <liubov.kostomarova@gmail.com>
2021-08-27 13:52:09 +0200

    Tasks uploaded

---
+++ Week14/task3/version2/Script/gallery_v2.js
@@ -0,0 +1,9 @@
+function prev() {
+    let pic1 = document.getElementById('image');
+    pic1.src = "Assets/Images/code.jpg";
+}
+
+function next() {
+    let pic3 = document.getElementById('image');
+    pic3.src = "Assets/Images/laptop.png";
+}
