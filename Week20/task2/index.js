7246682b4fa5e50e49d9f53131b5c58302f0ed74
Liubov Kostomarova <liubov.kostomarova@gmail.com>
2021-10-19 14:13:25 +0200

    Week20 added

--- Week20/task2/index.js
+++ Week20/task2/index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+/*document.addEventListener("DOMContentLoaded", function(){
     fetch('https://api.github.com/users/LiubovKostomarova')
         .then(response => response.json())
         .then(user => { 
@@ -15,4 +15,63 @@ document.getElementById('getAmount').addEventListener("click", function(){
             document.getElementById('amount').innerText = user.public_repos;
         })
         .catch(err => console.log(err));
-});
+});*/
+
+function sadFace() {
+    //alert(`I couldn't link the button to the Github info 😞`)
+}
+
+document.addEventListener("DOMContentLoaded",
+    function (event) {
+        showInfo();
+        showProjects();
+        showFollowers();
+    });
+
+function showInfo() {
+    fetch("https://api.github.com/users/LiubovKostomarova")
+        .then(response => (response.json()))
+
+                .then(user => {
+            document.getElementById("avatar").src = user.avatar_url;
+            document.getElementById("userName").innerText = user.name;
+            document.getElementById("bio").innerText = user.bio;
+        })
+        .catch(error => console.log(error));
+}
+
+//how make all of the projects to be shown?
+
+/*function showProjects() {
+    fetch("https://api.github.com/users/LiubovKostomarova/repos")
+        .then(response => (response.json()))
+
+        .then(list => {
+            document.getElementById("projects").innerText = list[0].name;
+        })
+        .catch(error => console.log(error));
+} 
+*/
+
+document.getElementById('getAmount').addEventListener("click", function(){
+    fetch('https://api.github.com/users/LiubovKostomarova')
+        .then(response => response.json())
+        .then(user => {
+            document.getElementById('amount').innerText = user.public_repos;
+            
+        })
+        .then(list => {
+            document.getElementById('projects').innerText = list[0].name;
+        })
+        .catch(err => console.log(err));
+})
+
+function showFollowers() {
+    fetch("https://api.github.com/users/LiubovKostomarova/followers")
+        .then(response => (response.json()))
+
+        .then(fol => {
+            document.getElementById("followers").innerText = fol[0].login;
+        })
+        .catch(error => console.log(error));
+}
